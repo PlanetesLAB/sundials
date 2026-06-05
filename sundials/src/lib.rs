4678ddc0b16bc8cb5f6ce82956354ff4ddf67dbd
kmaitreys <km.maitrey@gmail.com>
2026-06-05 10:11:10 +0530

    improve semantics and soundness of the public API

--- sundials/src/lib.rs
+++ sundials/src/lib.rs
@@ -35,7 +35,6 @@ impl SunContext {
     /// Panics if the context creation fails (i.e., if the returned pointer is null
     /// which should not happen under normal circumstances).
     #[must_use]
-    #[inline(always)]
     pub fn new() -> Self {
         let mut ctx = std::ptr::null_mut();
         unsafe { SUNContext_Create(SUN_COMM_NULL, &raw mut ctx) };
@@ -44,14 +43,14 @@ impl SunContext {
     }
 
     #[must_use]
-    #[inline(always)]
+    #[inline]
     pub fn as_raw(&self) -> SUNContext {
         self.ptr
     }
 }
 
 impl Drop for SunContext {
-    #[inline(always)]
+    #[inline]
     fn drop(&mut self) {
         unsafe { SUNContext_Free(&raw mut self.ptr) };
     }
@@ -65,43 +64,45 @@ pub struct NVector {
 impl NVector {
     /// Create a new serial [`NVector`] of the given length, associated with the provided [`SunContext`].
     ///
+    /// # Safety
+    /// The provided `SunContext` must strictly outlive the created `NVector`.
+    ///
     /// # Panics
     /// Panics if the vector creation fails (i.e., if the returned pointer is null
     /// which can happen if the length is zero or if there is an issue with the context).
     #[must_use]
-    #[inline(always)]
-    pub fn new_serial(len: usize, ctx: &SunContext) -> Self {
+    pub unsafe fn new_serial(len: usize, ctx: &SunContext) -> Self {
         let ptr = unsafe { N_VNew_Serial(len as i64, ctx.as_raw()) };
         assert!(!ptr.is_null());
         Self { ptr, len }
     }
 
     #[must_use]
-    #[inline(always)]
+    #[inline]
     pub fn as_raw(&self) -> N_Vector {
         self.ptr
     }
 
     #[must_use]
-    #[inline(always)]
+    #[inline]
     pub fn len(&self) -> usize {
         self.len
     }
 
     #[must_use]
-    #[inline(always)]
+    #[inline]
     pub fn is_empty(&self) -> bool {
         self.len == 0
     }
 
     #[must_use]
-    #[inline(always)]
+    #[inline]
     pub fn slice(&self) -> &[f64] {
         let p = unsafe { N_VGetArrayPointer_Serial(self.ptr) };
         unsafe { std::slice::from_raw_parts(p, self.len) }
     }
 
-    #[inline(always)]
+    #[inline]
     pub fn slice_mut(&mut self) -> &mut [f64] {
         let p = unsafe { N_VGetArrayPointer_Serial(self.ptr) };
         unsafe { std::slice::from_raw_parts_mut(p, self.len) }
@@ -109,7 +110,7 @@ impl NVector {
 }
 
 impl Drop for NVector {
-    #[inline(always)]
+    #[inline]
     fn drop(&mut self) {
         unsafe { N_VDestroy_Serial(self.ptr) };
     }
@@ -136,12 +137,14 @@ pub struct LinearSolver {
 impl LinearSolver {
     /// Create a new SPGMR linear solver for the given vector and context.
     ///
+    /// # Safety
+    /// The provided `SunContext` must strictly outlive the created `LinearSolver`.
+    ///
     /// # Panics
     /// Panics if the solver creation fails (i.e., if the returned pointer is null
     /// which can happen if there is an issue with the vector or context).
     #[must_use]
-    #[inline(always)]
-    pub fn spgmr(y: &NVector, ctx: &SunContext) -> Self {
+    pub unsafe fn spgmr(y: &NVector, ctx: &SunContext) -> Self {
         let solver = unsafe {
             SUNLinSol_SPGMR(
                 y.as_raw(),
@@ -156,12 +159,14 @@ impl LinearSolver {
 
     /// Create a new SPFGMR linear solver for the given vector and context.
     ///
+    /// # Safety
+    /// The provided `SunContext` must strictly outlive the created `LinearSolver`.
+    ///
     /// # Panics
     /// Panics if the solver creation fails (i.e., if the returned pointer is null
     /// which can happen if there is an issue with the vector or context).
     #[must_use]
-    #[inline(always)]
-    pub fn spfgmr(y: &NVector, ctx: &SunContext) -> Self {
+    pub unsafe fn spfgmr(y: &NVector, ctx: &SunContext) -> Self {
         let solver = unsafe {
             SUNLinSol_SPFGMR(
                 y.as_raw(),
@@ -176,12 +181,14 @@ impl LinearSolver {
 
     /// Create a new SPBCGS linear solver for the given vector and context.
     ///
+    /// # Safety
+    /// The provided `SunContext` must strictly outlive the created `LinearSolver`.
+    ///
     /// # Panics
     /// Panics if the solver creation fails (i.e., if the returned pointer is null
     /// which can happen if there is an issue with the vector or context).
     #[must_use]
-    #[inline(always)]
-    pub fn spbcgs(y: &NVector, ctx: &SunContext) -> Self {
+    pub unsafe fn spbcgs(y: &NVector, ctx: &SunContext) -> Self {
         let solver = unsafe {
             SUNLinSol_SPBCGS(
                 y.as_raw(),
@@ -196,12 +203,14 @@ impl LinearSolver {
 
     /// Create a new SPTFQMR linear solver for the given vector and context.
     ///
+    /// # Safety
+    /// The provided `SunContext` must strictly outlive the created `LinearSolver`.
+    ///
     /// # Panics
     /// Panics if the solver creation fails (i.e., if the returned pointer is null
     /// which can happen if there is an issue with the vector or context).
     #[must_use]
-    #[inline(always)]
-    pub fn sptfqmr(y: &NVector, ctx: &SunContext) -> Self {
+    pub unsafe fn sptfqmr(y: &NVector, ctx: &SunContext) -> Self {
         let solver = unsafe {
             SUNLinSol_SPTFQMR(
                 y.as_raw(),
@@ -215,14 +224,14 @@ impl LinearSolver {
     }
 
     #[must_use]
-    #[inline(always)]
+    #[inline]
     pub fn as_raw(&self) -> SUNLinearSolver {
         self.ptr
     }
 }
 
 impl Drop for LinearSolver {
-    #[inline(always)]
+    #[inline]
     fn drop(&mut self) {
         unsafe { SUNLinSolFree(self.ptr) };
     }
@@ -235,47 +244,49 @@ pub struct Cvode {
 impl Cvode {
     /// Create a new CVODE solver in BDF mode with the given context.
     ///
+    /// # Safety
+    /// The provided `SunContext` must strictly outlive the created `Cvode` solver.
+    ///
     /// # Panics
     /// Panics if the solver creation fails (i.e., if the returned pointer is null
     /// which can happen if there is an issue with the context).
     #[must_use]
-    #[inline(always)]
-    pub fn new_bdf(ctx: &SunContext) -> Self {
+    pub unsafe fn new_bdf(ctx: &SunContext) -> Self {
         let p = unsafe { CVodeCreate(CV_BDF, ctx.as_raw()) };
         assert!(!p.is_null());
         Self { ptr: p }
     }
 
     #[must_use]
-    #[inline(always)]
+    #[inline]
     pub fn as_raw(&self) -> *mut c_void {
         self.ptr
     }
 
-    #[inline(always)]
-    pub fn set_userdata<T>(&self, data: &mut T) {
+    /// Set user data.
+    ///
+    /// # Safety
+    /// The data referenced by `data` must outlive the `Cvode` solver instance.
+    /// Modifying or dropping the data while CVODE is still using it will cause undefined behavior.
+    pub unsafe fn set_userdata<T>(&mut self, data: &mut T) {
         unsafe {
             CVodeSetUserData(self.ptr, std::ptr::from_mut::<T>(data).cast::<c_void>());
         }
     }
 
-    #[inline(always)]
-    pub fn init(&self, rhs: CVRhsFn, t0: f64, y: &NVector) {
+    pub fn init(&mut self, rhs: CVRhsFn, t0: f64, y: &NVector) {
         unsafe { CVodeInit(self.ptr, rhs, t0, y.as_raw()) };
     }
 
-    #[inline(always)]
-    pub fn reinit(&self, t0: f64, y: &NVector) {
+    pub fn reinit(&mut self, t0: f64, y: &NVector) {
         unsafe { CVodeReInit(self.ptr, t0, y.as_raw()) };
     }
 
-    #[inline(always)]
-    pub fn set_tolerances(&self, reltol: f64, abstol: &NVector) {
+    pub fn set_tolerances(&mut self, reltol: f64, abstol: &NVector) {
         unsafe { CVodeSVtolerances(self.ptr, reltol, abstol.as_raw()) };
     }
 
-    #[inline(always)]
-    pub fn set_linear_solver(&self, linsol: &LinearSolver) {
+    pub fn set_linear_solver(&mut self, linsol: &LinearSolver) {
         unsafe { CVodeSetLinearSolver(self.ptr, linsol.as_raw(), std::ptr::null_mut()) };
     }
 
@@ -284,19 +295,17 @@ impl Cvode {
     /// # Errors
     /// Returns an error if the integration fails, which can happen for various reasons such as exceeding
     /// the maximum number of steps, convergence failures, or issues with the right-hand side function.
-    #[inline(always)]
-    pub fn integrate(&self, tout: f64, y: &mut NVector, t: &mut f64) -> Result<(), CvodeError> {
+    pub fn integrate(&mut self, tout: f64, y: &mut NVector, t: &mut f64) -> Result<(), CvodeError> {
         let retval = unsafe { CVode(self.ptr, tout, y.as_raw(), t, CV_NORMAL) };
         CvodeError::from_raw(retval)
     }
 
-    #[inline(always)]
-    pub fn integrate_one_step(&self, tout: f64, y: &NVector, t: &mut f64) -> i32 {
+    pub fn integrate_one_step(&mut self, tout: f64, y: &NVector, t: &mut f64) -> i32 {
         unsafe { CVode(self.ptr, tout, y.as_raw(), t, CV_ONE_STEP) }
     }
 
     #[must_use]
-    #[inline(always)]
+    #[inline]
     pub fn get_current_time(&self) -> f64 {
         let mut t = 0.0;
         unsafe { CVodeGetCurrentTime(self.ptr, &raw mut t) };
@@ -304,7 +313,7 @@ impl Cvode {
     }
 
     #[must_use]
-    #[inline(always)]
+    #[inline]
     pub fn get_last_step(&self) -> f64 {
         let mut h = 0.0;
         unsafe { CVodeGetLastStep(self.ptr, &raw mut h) };
@@ -312,102 +321,84 @@ impl Cvode {
     }
 
     #[must_use]
-    #[inline(always)]
+    #[inline]
     pub fn get_current_step(&self) -> f64 {
         let mut h = 0.0;
         unsafe { CVodeGetCurrentStep(self.ptr, &raw mut h) };
         h
     }
 
-    #[inline(always)]
-    pub fn set_preconditioner(&self, setup: CVLsPrecSetupFn, solve: CVLsPrecSolveFn) {
+    pub fn set_preconditioner(&mut self, setup: CVLsPrecSetupFn, solve: CVLsPrecSolveFn) {
         unsafe {
             CVodeSetPreconditioner(self.ptr, setup, solve);
         }
     }
 
-    #[inline(always)]
-    pub fn set_wf_tolerances(&self, efun: CVEwtFn) {
+    pub fn set_wf_tolerances(&mut self, efun: CVEwtFn) {
         unsafe { CVodeWFtolerances(self.ptr, efun) };
     }
 
-    #[inline(always)]
-    pub fn set_constraints(&self, constraints: &NVector) {
+    pub fn set_constraints(&mut self, constraints: &NVector) {
         unsafe { CVodeSetConstraints(self.ptr, constraints.as_raw()) };
     }
 
-    #[inline(always)]
-    pub fn set_max_nonlin_iters(&self, n: i32) {
+    pub fn set_max_nonlin_iters(&mut self, n: i32) {
         unsafe { CVodeSetMaxNonlinIters(self.ptr, n) };
     }
 
-    #[inline(always)]
-    pub fn set_max_conv_fails(&self, n: i32) {
+    pub fn set_max_conv_fails(&mut self, n: i32) {
         unsafe { CVodeSetMaxConvFails(self.ptr, n) };
     }
 
-    #[inline(always)]
-    pub fn set_eta_conv_fail(&self, eta: f64) {
+    pub fn set_eta_conv_fail(&mut self, eta: f64) {
         unsafe { CVodeSetEtaConvFail(self.ptr, eta) };
     }
 
-    #[inline(always)]
-    pub fn set_max_err_test_fails(&self, n: i32) {
+    pub fn set_max_err_test_fails(&mut self, n: i32) {
         unsafe { CVodeSetMaxErrTestFails(self.ptr, n) };
     }
 
-    #[inline(always)]
-    pub fn set_max_constraints_fails(&self, n: i32) {
+    pub fn set_max_constraints_fails(&mut self, n: i32) {
         unsafe { CVodeSetMaxNumConstraintFails(self.ptr, n) };
     }
 
-    #[inline(always)]
-    pub fn set_init_step(&self, h0: f64) {
+    pub fn set_init_step(&mut self, h0: f64) {
         unsafe { CVodeSetInitStep(self.ptr, h0) };
     }
 
-    #[inline(always)]
-    pub fn set_min_step(&self, hmin: f64) {
+    pub fn set_min_step(&mut self, hmin: f64) {
         unsafe { CVodeSetMinStep(self.ptr, hmin) };
     }
 
-    #[inline(always)]
-    pub fn set_max_step(&self, hmax: f64) {
+    pub fn set_max_step(&mut self, hmax: f64) {
         unsafe { CVodeSetMaxStep(self.ptr, hmax) };
     }
 
-    #[inline(always)]
-    pub fn set_max_ord(&self, ord: i32) {
+    pub fn set_max_ord(&mut self, ord: i32) {
         unsafe { CVodeSetMaxOrd(self.ptr, ord) };
     }
 
-    #[inline(always)]
-    pub fn set_max_num_steps(&self, n: i64) {
+    pub fn set_max_num_steps(&mut self, n: i64) {
         unsafe { CVodeSetMaxNumSteps(self.ptr, n) };
     }
 
-    #[inline(always)]
-    pub fn set_max_hnil_warns(&self, n: i32) {
+    pub fn set_max_hnil_warns(&mut self, n: i32) {
         unsafe { CVodeSetMaxHnilWarns(self.ptr, n) };
     }
 
-    #[inline(always)]
-    pub fn set_stability_limit_detection(&self, enable: i32) {
+    pub fn set_stability_limit_detection(&mut self, enable: i32) {
         unsafe { CVodeSetStabLimDet(self.ptr, enable) };
     }
 
-    #[inline(always)]
-    pub fn set_jac_eval_frequency(&self, freq: i64) {
+    pub fn set_jac_eval_frequency(&mut self, freq: i64) {
         unsafe { CVodeSetJacEvalFrequency(self.ptr, freq) };
     }
 
-    #[inline(always)]
-    pub fn set_linear_solver_setup_frequency(&self, freq: i64) {
+    pub fn set_linear_solver_setup_frequency(&mut self, freq: i64) {
         unsafe { CVodeSetLSetupFrequency(self.ptr, freq) };
     }
 
-    #[inline(always)]
-    pub fn set_epslin(&self, eps: f64) {
+    pub fn set_epslin(&mut self, eps: f64) {
         unsafe { CVodeSetEpsLin(self.ptr, eps) };
     }
 
@@ -418,7 +409,6 @@ impl Cvode {
     /// issues or if the filesystem is read-only.
     /// Panics if the CVODE statistics cannot be printed, which can happen if there is an internal error in the CVODE library.
     /// Note that this function will overwrite the file ``cvode_stats.txt`` if it already exists.
-    #[inline(always)]
     pub fn save_statistics(&self, fname: &str) {
         unsafe {
             let filename = CString::new(fname).unwrap();
